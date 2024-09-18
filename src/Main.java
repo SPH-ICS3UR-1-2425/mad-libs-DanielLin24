89ae915fa50a170c37d48d79225093c5b5a96209
DanielLin24 <htet.lin.yel1@stu.ocsb.ca>
2024-09-18 14:38:21 -0400

    Nearly finished
    almost done with the story I think

--- src/Main.java
+++ src/Main.java
@@ -19,14 +19,31 @@ public class Main {
 		String name = in.nextLine();
 		System.out.println("That's very nice... so nice that it matches the current suspects name");
 		System.out.println("Imma need your age now");
-		in.nextInt();
+		int age = in.nextInt();
 		in.nextLine();
-		System.out.println("Where were you last Thursday, at exactly 4 in the morning? (One word answer please) ");
+		System.out.println("Ok... Where were you last Thursday? (One word answer please) ");
 		String place = in.nextLine();
+		System.out.println("Uh huh, and at what time was this exactly? (Type the time like x.xx)");
+		double time = in.nextDouble();
+		in.nextLine();
+		System.out.println("What did you have on you that time? (One important item please)");
+		String item1 = in.nextLine();
 		System.out.println("Okay... I see, well im hungry and in a mood, sooooo, could you just confess? (Yes or No)");
-		String name2 = in.nextLine();
-		
-		
+		in.nextLine();
+		System.out.println("What? Was that a no??? Oh nah, you testing me huh");
+		System.out.println("Imma read your alibi right now, your name is " +name+ " you're " +age+ " you were at the (a) " +place+ " at " +time+ " right? (Yes or No)");
+		in.nextLine();
+		System.out.println("Yup and you also had a " +item1+ " and a knife, probably a bomb too, to be honest, I don't really care");
+		System.out.println("Soooo, let me finish your alibi real quick");
+		System.out.println("You were at (the) " +place+ " when you pulled out the " +item1+ " and hit Elijah Bicol with it, this unfortunately knocked him out, did you do it for real or nah?");
+		in.nextLine();
+		System.out.println("DON'T CARE BRO, you're not getting a trial and you're getting sentenced 40 years for murder with a " +item1+ " do you protest against this?");
+		in.nextLine();
+		System.out.println("For like the third time, I don't really care, I also don't know how to do if, else statements, so there are no other endings");
+		System.out.println("Well guys, hoped you liked this video");
+		System.out.println(" if you can, please like and hit that bell notification");
+		System.out.println("and if you're new to this channel, why not subscribe and as always");
+		System.out.println("see you in the next one :)");
 	}
 
 }
